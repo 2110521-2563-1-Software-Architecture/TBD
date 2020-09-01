39d790a40c51af74b660837c2d98423eebc86087
Setthanan Nakaphan <team1_team@hotmail.com>
2020-09-01 22:21:12 +0700

    add streaming

--- RestAPI/index.js
+++ RestAPI/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const events = require('events');
 
+const bookStream = new events.EventEmitter();
 let books = [];
+
 
 app.get('/books', (req,res) => {
     res.json(books)
@@ -16,15 +19,23 @@ app.get('/books/:id', (req,res) => {
 });
 
 app.post('/books', (req,res) => {
-    books.push(req.body)
-    console.log(req.body)
+    book = req.body
+    books.push(book)
+    bookStream.emit('new_book',book)
+    console.log(book)
     res.send('New book is added')
 });
 
 app.delete('/books/:id', (req,res) => {
-    const i = books.findIndex(book => book.id === req.params.id)
-    books.slice(i,1)
+    var i = books.findIndex(book => book.id === req.params.id)
+    books.splice(i,1)
     res.send('Delete book')
+})
+
+app.get('/streaming', (req,res) => {
+    bookStream.on('new_book',function(book){
+        res.write(book)
+    })
 })
 
 app.listen(3000, ()=>{
